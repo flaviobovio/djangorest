751c36283ceb3d687865f278b5620d0e4e91ce87
ozzy <flavioboviovt@gmail.com>
2025-05-22 20:17:11 -0300

    fix root / not found

--- swim/urls.py
+++ swim/urls.py
@@ -5,13 +5,13 @@ from rest_framework.documentation import include_docs_urls
 from .views import SwimmerViewSet, DateViewSet, MarkViewSet, ClubViewSet
 
 router = routers.DefaultRouter()
-
 router.register('club', ClubViewSet, 'club')
 router.register('swimmer', SwimmerViewSet, 'swimmer')
 router.register('date', DateViewSet, 'date')
 router.register('mark', MarkViewSet, 'mark')
 
 urlpatterns = [
+    path('', include(router.urls)),  
     path('api/v1/', include(router.urls)),
     path('docs/', include_docs_urls(title='Swim API'))
 ]
