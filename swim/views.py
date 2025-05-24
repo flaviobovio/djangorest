1337f35e25bb006591e0f2391c2d474cafb41a2d
ozzy <flavioboviovt@gmail.com>
2025-05-24 13:27:10 -0300

    permissions added to views

--- swim/views.py
+++ swim/views.py
@@ -4,30 +4,28 @@ from .models import Club, Swimmer, Date, Mark
 from .serializers import ClubSerializer, SwimmerSerializer, DateSerializer, MarkSerializer
 
 
-
-
 # Create your views here
 class ClubViewSet(viewsets.ModelViewSet):
     """ Club  ViewSet """
     queryset = Club.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = ClubSerializer    
 
 
 class SwimmerViewSet(viewsets.ModelViewSet):
     """ Swimmer ViewSet """
     queryset = Swimmer.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = SwimmerSerializer
 
 class DateViewSet(viewsets.ModelViewSet):
     """ Date ViewSet """
     queryset = Date.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = DateSerializer
 
 class MarkViewSet(viewsets.ModelViewSet):
     """ Mark ViewSet """
     queryset = Mark.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = MarkSerializer
