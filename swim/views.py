bf948a33fff62fed03a02e2d15d6b8fb110ce34b
ozzy <flavioboviovt@gmail.com>
2025-05-31 00:19:05 -0300

    add djangoModel permissions to views

--- swim/views.py
+++ swim/views.py
@@ -1,31 +1,32 @@
 """  ViewSets """
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets
 from .models import Club, Swimmer, Date, Mark
 from .serializers import ClubSerializer, SwimmerSerializer, DateSerializer, MarkSerializer
+from rest_framework import permissions
 
 
 # Create your views here
 class ClubViewSet(viewsets.ModelViewSet):
     """ Club  ViewSet """
     queryset = Club.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.DjangoModelPermissions]
     serializer_class = ClubSerializer    
 
 
 class SwimmerViewSet(viewsets.ModelViewSet):
     """ Swimmer ViewSet """
     queryset = Swimmer.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.DjangoModelPermissions]
     serializer_class = SwimmerSerializer
 
 class DateViewSet(viewsets.ModelViewSet):
     """ Date ViewSet """
     queryset = Date.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.DjangoModelPermissions]
     serializer_class = DateSerializer
 
 class MarkViewSet(viewsets.ModelViewSet):
     """ Mark ViewSet """
     queryset = Mark.objects.all()
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.DjangoModelPermissions]
     serializer_class = MarkSerializer
